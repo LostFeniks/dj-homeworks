ed392bf62bb24c8e4f4aef41936ff4e7e19476e2
a.burtseva <a.burtseva@malltech-lab.ru>
2025-10-13 17:26:06 +0300

    2.2/m2m

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "Раздел"
+        verbose_name_plural = "Разделы"
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    tags = models.ManyToManyField(Tag, through='Scope', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +25,20 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def ordered_scopes(self):
+        """Возвращает scopes: основной тег первым, остальные по алфавиту"""
+        return self.scopes.order_by('-is_main', 'tag__name')
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('article', 'tag')
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name} {'(Main)' if self.is_main else ''}"
+
